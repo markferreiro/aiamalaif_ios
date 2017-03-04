23f37877020eadd6370707400b83598c64feca86
Oriol Marí Marqués <Oriol@Oriols-MacBook-Pro.local>
2017-03-04 02:12:15 +0100

    Initial Commit

---
+++ aimalaif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aimalaif
+//
+//  Created by Oriol Marí Marqués on 04/03/2017.
+//  Copyright © 2017 Oriol Marí Marqués. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
